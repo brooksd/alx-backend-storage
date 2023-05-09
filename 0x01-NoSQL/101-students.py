8124c1cd8bad1b0b3390a8a593b0f859e7ebda3f
Donald Brooks <brooksd079@gmail.com>
2023-05-09 13:39:49 +0300

    all students sorted by average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-"""14. Top students"""
+""" MongoDB Operations with Python using pymongo """
 
 
 def top_students(mongo_collection):
-    students = mongo_collection.find()
-    for student in students:
-        topics = student.get('topics', [])
-        size = len(topics)
-        accumulated = 0
-        for topic in topics:
-            accumulated += topic.get('score')
-        avg = accumulated / size
-        mongo_collection.update_many(
-            { 'name': student.get('name') },
-            { '$set': { 'averageScore': avg } }
-        )
+    # sourcery skip: inline-immediately-returned-variable
+    """ Returns all students sorted by average score """
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
 
-    return mongo_collection.find().sort('averageScore', -1)
+    return top_student
