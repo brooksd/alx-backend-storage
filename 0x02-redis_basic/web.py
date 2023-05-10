14aef4ddf4091ab168767a2516156480b606c422
Donald Brooks <brooksd079@gmail.com>
2023-05-10 15:10:55 +0300

    Implementing expiring web cache and tracker for online sessions

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Redis Module Imports """
+
+from functools import wraps
+import redis
+import requests
+from typing import Callable
+
+redis_ = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(url):  '''use named-expression'''
+        """ decorator wrapper """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
+def get_page(url: str) -> str:
+    """ Fetch HTML content from an endpoint"""
+    req = requests.get(url)
+    return req.text
